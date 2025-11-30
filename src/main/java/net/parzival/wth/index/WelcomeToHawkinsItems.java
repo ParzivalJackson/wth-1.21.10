a4301630102e03c2352a6cb57ef9536e563d7ee4
EpikGamerPG <epikgamerpg@gmail.com>
2025-11-30 03:02:08 +0100

    added things and changes

--- src/main/java/net/parzival/wth/index/WelcomeToHawkinsItems.java
+++ src/main/java/net/parzival/wth/index/WelcomeToHawkinsItems.java
@@ -3,20 +3,29 @@ package net.parzival.wth.index;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.registry.*;
 import net.minecraft.util.Identifier;
 import net.parzival.wth.WelcomeToHawkins;
 
 public class WelcomeToHawkinsItems {
 
-    public static final Item NAIL_BAT = registerItem(
-            "nail_bat",
-            new Item(new Item.Settings())
-    );
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(
+    public static final Item NAIL_BAT = create("nail_bat");
+    public static final Item NAILS = create("nails");
+
+
+    private static Item create(String name) {
+        // Pre-set registry key (fixes the 1.21.10 null-ID crash)
+        RegistryKey<Item> key = RegistryKey.of(
+                RegistryKeys.ITEM,
+                Identifier.of(WelcomeToHawkins.MOD_ID, name)
+        );
+
+        return new Item(new Item.Settings().registryKey(key));
+    }
+
+    private static void registerItem(String name, Item item) {
+        Registry.register(
                 Registries.ITEM,
                 Identifier.of(WelcomeToHawkins.MOD_ID, name),
                 item
@@ -24,8 +33,12 @@ public class WelcomeToHawkinsItems {
     }
 
     public static void registerWelcomeToHawkinsItems() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries ->
-                entries.add(NAIL_BAT)
-        );
+        registerItem("nail_bat", NAIL_BAT);
+        registerItem("nails", NAILS);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(NAIL_BAT);
+            entries.add(NAILS);
+        });
     }
 }
